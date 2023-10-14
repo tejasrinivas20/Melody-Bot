21da9ca101de6aa2e6690917cdd5fa1760c0378c
tejasrinivas20 <g.tejasrinivas1234@gmail.com>
2023-10-14 11:09:00 +0530

    Added a test app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+import praw
+
+reddit = praw.Reddit(
+    client_id="your_client_id",
+    client_secret="your_client_secret",
+    username="your_username",
+    password="your_password",
+    user_agent="your_user_agent",
+)
